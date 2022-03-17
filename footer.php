5ed8fa162dd776f60b10b7c9c0cf159c4dcda4c4
Jhonata Medeiros da Silva <jhonatams@gmail.com>
2022-03-17 20:01:49 -0300

    Center footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
-<footer>
-    Comando Certo &copy; 2013-<?= date("Y") ?> - Todos os direitos reservados
+<footer class="text-muted text-center d-block fixed-bottom">
+    &copy; Comando Certo <small>2013~<?= date("Y") ?></small>
 </footer>
 </body>
 <?php wp_footer(); ?>
