3f10225c0d79dbdfd7123314920778c394014df0
Jhonata Medeiros da Silva <jhonatams@gmail.com>
2022-03-17 14:53:58 -0300

    Add estilo ao post-thumbnail

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-$estiloPagina = 'genenal.css';
+$stylePage = 'index.css';
 require_once 'header.php';
 
 the_custom_logo();
 
 while (have_posts()): the_post();
-    the_post_thumbnail('post-thumbnail', array('class' => ''));
+    the_post_thumbnail('post-thumbnail', array('class' => 'cc-post-thumbnail'));
     the_title('<h2>', '</h2>');
     the_content();
 endwhile;
