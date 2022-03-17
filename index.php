a0eddc54993395419acaa837f6ce952a89b4b5de
Jhonata Medeiros da Silva <jhonatams@gmail.com>
2022-03-17 20:01:16 -0300

    Add navbar

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 $stylePage = 'index.css';
 require_once 'header.php';
-
-the_custom_logo();
+require_once 'navbar.php';
 
 while (have_posts()): the_post();
     the_post_thumbnail('post-thumbnail', array('class' => 'cc-post-thumbnail'));
+    echo '<content class="cc-content">';
     the_title('<h2>', '</h2>');
     the_content();
+    echo '</content>';
 endwhile;
 
 require_once 'footer.php';
