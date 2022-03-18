ed9df11764731d7b2680eb73662629e58f2503fa
Jhonata Medeiros da Silva <jhonatams@gmail.com>
2022-03-18 17:50:54 -0300

    Unifica tudo no index.php

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 $stylePage = 'index.css';
 require_once 'header.php';
 require_once 'navbar.php';
+$custom = get_post_custom();
+?>
 
+<style>
+.cc-post-thumbnail {
+    object-position: <?= $custom['object-position'][0] ?> !important;
+}
+</style>
+
+<?php
 while (have_posts()): the_post();
     the_post_thumbnail('post-thumbnail', array('class' => 'cc-post-thumbnail'));
     echo '<content class="cc-content">';
-    the_title('<h2>', '</h2>');
+    if (get_page_uri() != 'home') {
+        the_title('<h2>', '</h2>');
+    }
     the_content();
     echo '</content>';
 endwhile;
