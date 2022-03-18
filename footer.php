da6feae310b2a4b6b241d11ed60c57b7bedeed18
Jhonata Medeiros da Silva <jhonatams@gmail.com>
2022-03-18 16:43:58 -0300

    Esconde rodapé

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-<footer class="text-muted text-center d-block fixed-bottom">
+<!-- <footer class="text-muted text-center d-block fixed-bottom">
     &copy; Comando Certo <small>2013~<?= date("Y") ?></small>
-</footer>
+</footer> -->
 </body>
 <?php wp_footer(); ?>
 </html>
